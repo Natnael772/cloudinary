6d855710ace04e64f254f67b5d48b919d6b77d21
natnael772 <natnaeldeyas0@gmail.com>
2024-05-23 00:19:43 +0300

    feat: cloudinary

---
+++ src/cloudinary/cloudinary.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { CloudinaryProvider } from './cloudinary.provider';
+import { CloudinaryService } from './cloudinary.service';
+import { MulterModule } from '@nestjs/platform-express';
+
+@Module({
+  imports: [
+    MulterModule.register({
+      dest: './uploads',
+    }),
+  ],
+  providers: [CloudinaryProvider, CloudinaryService],
+  exports: [CloudinaryProvider, CloudinaryService],
+})
+export class CloudinaryModule {}
