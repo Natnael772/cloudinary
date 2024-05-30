b455ddd89590d59742b9a319ddeece1cb7c38e88
natnael772 <natnaeldeyas0@gmail.com>
2024-05-30 20:22:08 +0300

    feat: add image uploading functionality

--- src/cloudinary/cloudinary.service.ts
+++ src/cloudinary/cloudinary.service.ts
@@ -18,4 +18,62 @@ export class CloudinaryService {
       toStream(file.buffer).pipe(upload);
     });
   }
+
+  async getUploadedFiles() {
+    return new Promise((resolve, reject) => {
+      v2.api.resources(
+        {
+          type: 'upload',
+          prefix: IMAGE_FOLDER,
+        },
+        (error, result) => {
+          if (error) return reject(error);
+          resolve(result.resources);
+        },
+      );
+    });
+  }
+
+  async getFileByPublicId(assetId: string) {
+    return new Promise((resolve, reject) => {
+      v2.api.resource(assetId, (error, result) => {
+        if (error) return reject(error);
+        resolve(result);
+      });
+    });
+  }
+
+  async uploadVideo(
+    file: Express.Multer.File,
+  ): Promise<UploadApiResponse | UploadApiErrorResponse> {
+    return new Promise((resolve, reject) => {
+      const upload = v2.uploader.upload_stream(
+        { resource_type: 'video' },
+        (error, result) => {
+          if (error) return reject(error);
+          resolve(result);
+        },
+      );
+      toStream(file.buffer).pipe(upload);
+    });
+  }
+
+  async uploadMultipleImages(
+    files: Express.Multer.File[],
+  ): Promise<UploadApiResponse[] | UploadApiErrorResponse> {
+    return Promise.all(
+      files.map((file) => {
+        return new Promise((resolve, reject) => {
+          const upload = v2.uploader.upload_stream(
+            { folder: IMAGE_FOLDER },
+            (error, result) => {
+              if (error) return reject(error);
+              resolve(result);
+            },
+          );
+          toStream(file.buffer).pipe(upload);
+        });
+      }),
+    ) as Promise<UploadApiResponse[] | UploadApiErrorResponse>;
+  }
 }
--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -2,9 +2,12 @@ import {
   BadRequestException,
   Body,
   Controller,
+  Get,
+  Param,
   Post,
   Req,
   UploadedFile,
+  UploadedFiles,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
@@ -14,13 +17,62 @@ import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
 export class FileController {
   constructor(private readonly cloudinaryService: CloudinaryService) {}
 
-  @Post('upload')
+  @Post('upload/image')
   @UseInterceptors(FileInterceptor('image'))
   async uploadImageToCloudinary(@UploadedFile() file: Express.Multer.File) {
     return await this.cloudinaryService.uploadImage(file).catch((error) => {
       console.log(error);
       throw new BadRequestException(
         error.message || 'Error uploading image to Cloudinary',
+      );
+    });
+  }
+
+  // @Post('upload/images')
+  // @UseInterceptors(FileInterceptor('images'))
+  // async uploadMultipleImagesToCloudinary(
+  //   @UploadedFiles() files: Express.Multer.File[],
+  // ) {
+  //   return await this.cloudinaryService
+  //     .uploadMultipleImages(files)
+  //     .catch((error) => {
+  //       console.log(error);
+  //       throw new BadRequestException(
+  //         error.message || 'Error uploading image to Cloudinary',
+  //       );
+  //     });
+  // }
+
+  @Get('find')
+  async getFileByPublicId(@Body() { public_id }: { public_id: string }) {
+    console.log(public_id);
+    return await this.cloudinaryService
+      .getFileByPublicId(public_id)
+      .catch((error) => {
+        console.log(error);
+        throw new BadRequestException(
+          error.message || 'Error fetching file from Cloudinary',
+        );
+      });
+  }
+
+  @Get('all')
+  async getFiles() {
+    return await this.cloudinaryService.getUploadedFiles().catch((error) => {
+      console.log(error);
+      throw new BadRequestException(
+        error.message || 'Error fetching files from Cloudinary',
+      );
+    });
+  }
+
+  @Post('upload/video')
+  @UseInterceptors(FileInterceptor('video'))
+  async uploadVideoToCloudinary(@UploadedFile() file: Express.Multer.File) {
+    return await this.cloudinaryService.uploadVideo(file).catch((error) => {
+      console.log(error);
+      throw new BadRequestException(
+        error.message || 'Error uploading video to Cloudinary',
       );
     });
   }
